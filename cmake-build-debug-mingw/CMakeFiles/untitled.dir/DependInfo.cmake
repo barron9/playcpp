62e1a2054ea19c3f22bfe67d79450004d07b1554
btatlisu <025186a>
2020-12-09 00:17:19 +0300

    init

---
+++ cmake-build-debug-mingw/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/berkin1/CLionProjects/untitled/main.cpp" "C:/Users/berkin1/CLionProjects/untitled/cmake-build-debug-mingw/CMakeFiles/untitled.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CURL_STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "D:/curl-7.73.0_5-win64-mingw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
